ddb50015d2c0e4c5c5320972bd2598b9fb2d7fa5
Basliel Selamu <baslielselamu2018@gmail.com>
2021-12-10 18:30:18 +0300

    counting valley

---
+++ Week-01-09-java/NumberOfGoodPair.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Basliel
+ */
+public class NumberOfGoodPair {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a[] = new int[101];
+        System.out.println(a[2]);
+    }
+
+    public 
+    
+}
