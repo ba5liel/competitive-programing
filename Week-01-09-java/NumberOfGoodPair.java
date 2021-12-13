a08f703b201b684833b9c0d766630c35a7f858d0
Basliel Selamu <baslielselamu2018@gmail.com>
2021-12-13 17:49:36 +0300

    Bubble sort

--- Week-01-09-java/NumberOfGoodPair.java
+++ Week-01-09-java/NumberOfGoodPair.java
@@ -9,10 +9,18 @@ public class NumberOfGoodPair {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int a[] = new int[101];
-        System.out.println(a[2]);
+    
+        System.out.println(getNumberOfGoodPair(new int[] {1,2,1,1,2,2}));
     }
 
-    public 
-    
+    public static int getNumberOfGoodPair(int[] A) {
+        int count[] = new int[101];
+        int result = 0;
+        for (int a : A) {
+           result = result + count[a]++;
+           System.out.println(String.format("new result %s",result));
+        }
+        return result;
+    }
+
 }
